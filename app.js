3f042a0883f26f2021e6b3ea6f6fde03c080755c
Ap0c <Ap0c@users.noreply.github.com>
2016-08-14 17:39:47 +0100

    Set up mithril routing, and created components for movies and tv shows pages.

--- app.js
+++ app.js
@@ -7,24 +7,41 @@ let main = document.getElementsByTagName('main')[0];
 
 let menu = {
 
-	controller: function () {},
-
 	view: function (ctrl) {
 
 		return [
-			m('a', [
-				m('h2', 'Movies')
-			]),
-			m('a', [
-				m('h2', 'TV Shows')
-			])
+			m('a[href="/movies"]', { config: m.route }, 'Movies'),
+			m('a[href="/tv_shows"]', { config: m.route }, 'TV Shows')
 		];
 
 	}
 
 };
 
+let movies = {
 
-// ----- Mount ----- //
+	view: function (ctrl) {
+		console.log('here');
+		return 'Here are some movies...';
+	}
 
-m.mount(main, menu);
+};
+
+let tvShows = {
+
+	view: function (ctrl) {
+		return 'Here are some tv shows...';
+	}
+
+};
+
+
+// ----- Routing ----- //
+
+m.route.mode = 'pathname';
+
+m.route(main, '/', {
+	'/': menu,
+	'/movies': movies,
+	'/tv_shows': tvShows
+});
